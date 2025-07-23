df60759f4817f960c3310f8178a085e06bbfc6b4
gragra-kj <muthuigracia27@gmail.com>
2025-07-24 00:50:41 +0300

    models

--- chatter/models.py
+++ chatter/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
 # Create your models here.
+from django.contrib.auth.models import User
+
+class ChatroomModel(models.Model):
+    name=models.CharField(max_length=100)
+    participant=models.ForeignKey(User,on_delete=models.CASCADE)
+    created_at=models.DateTimeField(auto_now_add=True)
+    updated_at=models.DateTimeField(auto_now_add=True)
+    
+class MessageModel(models.Model):
+    chat_room=models.ForeignKey(ChatroomModel,on_delete=models.CASCADE)
+    sender=models.ForeignKey(User,on_delete=models.CASCADE)
+    content=models.TextField()
+    time_stamp=models.DateTimeField(auto_now_add=True)
+    read=models.BooleanField(default=False)
+    
+class MessageStatus(models.Model):
+    message=models.ForeignKey(MessageModel,on_delete=models.CASCADE)
+    user=models.ForeignKey(User,on_delete=models.CASCADE)
+    is_read=models.BooleanField(default=False)
+    read_at=models.DateTimeField(auto_now_add=True)        
