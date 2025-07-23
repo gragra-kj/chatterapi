9de5053bfea34a4a6921e851ed1f11df40fff37e
gragra-kj <muthuigracia27@gmail.com>
2025-07-24 02:03:10 +0300

    serializers and viewsets

---
+++ chatter/serializers.py
@@ -0,0 +1,34 @@
+from rest_framework import serializers
+from .models import ChatroomModel,MessageModel,MessageStatus
+from django.contrib.auth.models import User
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['id', 'username']
+
+
+class ChatRoomSerializer(serializers.ModelSerializer):
+    participant = UserSerializer(many=True, read_only=True)
+    class Meta:
+        model=ChatroomModel
+        fields=['id','name','participant','created_at','updated_at']
+        
+class MessageSerializer(serializers.ModelSerializer):
+    sender = serializers.ReadOnlyField(source='sender.username')  
+    chat_room = serializers.PrimaryKeyRelatedField(queryset=ChatroomModel.objects.all())
+    class Meta:
+        model = MessageModel
+        fields = ['id', 'chat_room', 'sender', 'content', 'time_stamp', 'read']
+
+    def create(self, validated_data):
+        user = self.context['request'].user  
+        validated_data['sender'] = user
+        return super().create(validated_data)
+        
+class MessageStatusSerializer(serializers.ModelSerializer):
+    message = MessageSerializer(read_only=True)
+    user = UserSerializer(read_only=True)
+    class Meta:
+        model=MessageStatus
+        fields=['id','message','user','is_read','read_at']        
--- chatter/views.py
+++ chatter/views.py
@@ -1,3 +1,26 @@
 from django.shortcuts import render
 
 # Create your views here.
+from rest_framework import viewsets,permissions
+from .models import MessageModel,MessageStatus,ChatroomModel
+from .serializers import MessageSerializer,MessageStatusSerializer,ChatRoomSerializer
+
+class ChatRoomViewSet(viewsets.ModelViewSet):
+    queryset=ChatroomModel.objects.all()
+    serializer_class=ChatRoomSerializer
+    permission_classes=[permissions.IsAuthenticated]
+    
+    
+class MessageViewSet(viewsets.ModelViewSet):
+    queryset = MessageModel.objects.all()
+    serializer_class = MessageSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def perform_create(self, serializer):
+        serializer.save(sender=self.request.user)
+class MessageStatusViewSet(viewsets.ModelViewSet):
+    queryset=MessageStatus.objects.all()
+    serializer_class=MessageStatusSerializer
+    permission_classes=[permissions.IsAuthenticated]
+        
+        
