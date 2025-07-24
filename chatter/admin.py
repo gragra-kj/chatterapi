d0266a7b577406cd1612789b14b837bd8bb4b21e
gragra-kj <muthuigracia27@gmail.com>
2025-07-24 21:30:09 +0300

    register models

--- chatter/admin.py
+++ chatter/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import ChatroomModel,MessageModel,MessageStatus
+
+admin.site.register(ChatroomModel)
+admin.site.register(MessageModel)
+admin.site.register(MessageStatus)
